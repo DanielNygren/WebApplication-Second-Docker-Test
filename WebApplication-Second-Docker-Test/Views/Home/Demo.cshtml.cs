da8eab1c137d7281db274eea00e0e3c68cb330f2
DanielNygren <75730040+DanielNygren@users.noreply.github.com>
2022-02-23 21:00:36 +0100

    added demo 2202232100

---
+++ WebApplication-Second-Docker-Test/Views/Home/Demo.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_Second_Docker_Test.Views.Home
+{
+    public class DemoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
